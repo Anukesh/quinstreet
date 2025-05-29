76feefb5e69a77c142a1d59cb3d18696543db87f
Anukesh Bhawsar <anukeshbhawsar@Anukeshs-MacBook-Pro.local>
2025-05-29 22:52:26 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,133 @@
+import "./style.css";
+import modernizeLogo from "/logo.svg";
+
+document.querySelector("#app").innerHTML = `
+  <div class="container">
+    <header class="header">
+      <img src=${modernizeLogo} alt="Modernize Logo" class="logo" />
+    </header>
+
+    <section class="form-section">
+      <div class="form-box">
+        <div class="form-title-container">
+        <p class="form-title">Enter to win a 3rd generation Nest Learning Thermostat worth $249.</p>
+        </div>
+        <form id="userForm">
+          <input id="name"  type="text" placeholder="Name" required minlength="2"/>
+          <div class="city-state">
+            <input id="city" type="text" class="city-input" placeholder="City" />
+            <input id="state" type="text" class="state-input" placeholder="State" />
+          </div>
+          <input id="phone" type="text" placeholder="Phone Number" required />
+          <input id="email" type="email" placeholder="Email Address" required />
+          <button id="submit" class="submit-button " type="submit">Enter to Win</button>
+        </form>
+      </div>
+    </section>
+
+    <section class="advertorial">
+    <div class="ad-container">
+    <div class="sub-header-container">
+    <p class="sub-header">Even More Savings</p>
+    </div>
+    <div class="ad-box">
+    <div class="ad-box-inner">
+    <div>
+    <p class="ad-label">ADVERTORIAL</p>
+    <h3 class="ad-heading">Tricks Homeowners Use to Eliminate Bills</h3>
+    </div>
+    <div>
+    <p class="ad-text">Surging energy bills, unpredictable weather patterns and dissatisfaction with utility companies have homeowners scrambling...</p>
+    <a href="https://www.google.com" target="_blank">READ MORE</a>
+    </div>
+    </div>
+    </div>
+    </div>
+    </section>
+  </div>
+`;
+
+const nameInput = document.querySelector("#name");
+const cityInput = document.querySelector("#city");
+const stateInput = document.querySelector("#state");
+const phoneInput = document.querySelector("#phone");
+const emailInput = document.querySelector("#email");
+const form = document.querySelector("#userForm");
+const submitBtn = document.querySelector("#submit");
+
+phoneInput.addEventListener("input", handlePhoneInput);
+emailInput.addEventListener("input", handleEmailInput);
+form.addEventListener("input", handleFormInput);
+form.addEventListener("submit", handleFormSubmit);
+
+document.querySelectorAll("input").forEach((input) => {
+  input.addEventListener("blur", () => {
+    input.classList.add("touched");
+  });
+});
+
+function handlePhoneInput(e) {
+  let value = e.target.value.replace(/\D/g, "");
+
+  if (value.length > 10) value = value.slice(0, 10);
+
+  let formatted = "";
+
+  if (value.length > 0) formatted += "(" + value.slice(0, 3);
+  if (value.length >= 4) formatted += ") " + value.slice(3, 6);
+  if (value.length >= 7) formatted += "-" + value.slice(6, 10);
+
+  e.target.value = formatted;
+
+  const isValid = /^\(\d{3}\) \d{3}-\d{4}$/.test(e.target.value);
+
+  if (!isValid) {
+    e.target.setCustomValidity("Invalid phone format");
+  } else {
+    e.target.setCustomValidity("");
+  }
+}
+
+function handleEmailInput(e) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(e.target.value)) {
+    e.target.setCustomValidity("Invalid phone format");
+  } else {
+    e.target.setCustomValidity("");
+  }
+}
+
+function handleFormInput() {
+  console.log(form.checkValidity());
+  submitBtn.disabled = !form.checkValidity();
+}
+
+async function handleFormSubmit(event) {
+  event.preventDefault();
+  submitBtn.disabled = true;
+  submitBtn.innerHTML = "Submitted!";
+
+  const data = {
+    name: nameInput.value,
+    city: cityInput.value,
+    state: stateInput.value,
+    phone: phoneInput.value,
+    email: emailInput.value,
+  };
+
+  try {
+    const response = await fetch(
+      "https://formsws-hilstaging-com-0adj9wt8gzyq.runscope.net/solar",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    console.log("Form submitted successfully:");
+  } catch (error) {
+    console.error("Submission failed:");
+  }
+}
